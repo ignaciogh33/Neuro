ab1b43c2b5b6d19372cb04c499d387df1a7f8387
Marco Gomez Hernandez <marco.gomezh@estudiante.uam.es>
2026-02-23 22:50:34 +0100

    a

--- p1/src/mp.py
+++ p1/src/mp.py
@@ -1,5 +1,8 @@
 from redes_neuronales import Red, Capa, NeuronaEntrada, NeuronaMcCullochPitts
 import sys
+import csv
+import pandas as pd
+import numpy as np
 
 
 def main():
@@ -12,16 +15,18 @@ def main():
 
     x1 = NeuronaEntrada()
     x2 = NeuronaEntrada()
-    z1 = NeuronaMcCullochPitts(1)
-    z2 = NeuronaMcCullochPitts(1)
-    y1 = NeuronaMcCullochPitts(1)
-    y2 = NeuronaMcCullochPitts(1)
+    z1 = NeuronaMcCullochPitts(2)
+    z2 = NeuronaMcCullochPitts(2)
+    y1 = NeuronaMcCullochPitts(2)
+    y2 = NeuronaMcCullochPitts(2)
 
+    x1.Conectar(y1, 2)
+    x2.Conectar(z1, -1)
     x2.Conectar(z2, 2)
-    z2.Conectar(z1, 2)
-    z1.Conectar(y1, 2)
-    z2.Conectar(y2, 1)
     x2.Conectar(y2, 1)
+    z2.Conectar(z1, 2)
+    z2.Conectar(y2, 1)
+    z1.Conectar(y1, 2)
 
     red = Red()
 
@@ -29,29 +34,25 @@ def main():
     capa_entrada.Añadir(x1)
     capa_entrada.Añadir(x2)
 
-    capa_oculta = Capa()
-    capa_oculta.Añadir(z2)
-    capa_oculta.Añadir(z1)
+    capa_oculta1 = Capa()
+    capa_oculta1.Añadir(z2)
+
+    capa_oculta2 = Capa()
+    capa_oculta2.Añadir(z1)
 
     capa_salida = Capa()
     capa_salida.Añadir(y1)
     capa_salida.Añadir(y2)
 
     red.Añadir(capa_entrada)
-    red.Añadir(capa_oculta)
+    red.Añadir(capa_oculta1)
+    red.Añadir(capa_oculta2)
     red.Añadir(capa_salida)
 
     red.Inicializar(0)
 
-    with open(entrada, 'r') as f:
-        lineas = f.readlines()
-
-    patrones = []
-    for linea in lineas:
-        linea = linea.strip()
-        if linea:
-            vals = list(map(int, linea.split()))
-            patrones.append(vals)
+    patrones = pd.read_csv(entrada, sep=r'\s+',
+                           header=None, engine='c').to_numpy()
 
     resultados = []
 
@@ -59,26 +60,24 @@ def main():
         x1.entrada = patron[0]
         x2.entrada = patron[1]
         red.Disparar()
-        resultados.append([int(x1.valor), int(x2.valor), int(
-            z1.valor), int(z2.valor), int(y1.valor), int(y2.valor)])
+        resultados.append([x1.valor, x2.valor,
+                           z1.valor, z2.valor, y1.valor, y2.valor])
         red.Propagar()
 
     while True:
         x1.entrada = 0
         x2.entrada = 0
         red.Disparar()
-
         if z1.valor == 0 and z2.valor == 0 and y1.valor == 0 and y2.valor == 0:
             break
-
-        resultados.append([int(x1.valor), int(x2.valor), int(
-            z1.valor), int(z2.valor), int(y1.valor), int(y2.valor)])
+        resultados.append([x1.valor, x2.valor,
+                           z1.valor, z2.valor, y1.valor, y2.valor])
         red.Propagar()
 
-    with open(salida, 'w') as f:
-        f.write("x1 x2 z1 z2 y1 y2\n")
-        for r in resultados:
-            f.write(" ".join(map(str, r)) + "\n")
+    with open(salida, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=' ')
+        writer.writerow(["x1", "x2", "z1", "z2", "y1", "y2"])
+        writer.writerows(resultados)
 
 
 if __name__ == '__main__':
